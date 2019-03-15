5fca8469922f9c2beb88b005daf450d5c17ef506
sanesco <santiagoescobar11z@gmail.com>
2019-03-15 15:51:28 -0500

    Primer proyecto sinatra

---
+++ solution.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+get '/' do
+  if params[:nombre].nil? || params[:nombre].empty?
+  "<h1>Hola desconocido!</h1>"
+  else
+  "<h1>Hola #{params[:nombre]}</h1>"
+  end
+end
+
+get '/:nombre' do
+  "<h1>Hola #{params[:nombre]}</h1>"
+end
