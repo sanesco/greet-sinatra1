7f767d186dc9b4d6d284a0f4663e16b85f7e6960
sanesco <santiagoescobar11z@gmail.com>
2019-03-15 18:23:55 -0500

    saludame 2

--- solution.rb
+++ solution.rb
@@ -1,12 +1,26 @@
-require 'sinatra'
-get '/' do
-  if params[:nombre].nil? || params[:nombre].empty?
-  "<h1>Hola desconocido!</h1>"
-  else
-  "<h1>Hola #{params[:nombre]}!</h1>"
-  end
-end
+# require 'sinatra'
+# get '/' do
+#   if params[:nombre].nil? || params[:nombre].empty?
+#   "<h1>Hola desconocido!</h1>"
+#   else
+#   "<h1>Hola #{params[:nombre]}!</h1>"
+#   end
+# end
+#
+# get '/:nombre' do
+#   "<h1>Hola #{params[:nombre]}</h1>"
+# end
 
-get '/:nombre' do
-  "<h1>Hola #{params[:nombre]}</h1>"
-end
+require 'sinatra'
+
+get '/makers/:nombre' do
+   if params[:nombre].nil? || params[:nombre].empty?
+   "<h1>Hola desconocido!</h1>"
+   else
+   "<h1>Hola #{params['nombre']}!</h1>"
+   end
+ end
+
+ get '/:nombre' do
+   "<h1>Hola #{params[:nombre]}</h1>"
+ end
