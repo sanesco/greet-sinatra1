c4c9e2fbef109e13f2d78957290a4b40824b2ad8
sanesco <santiagoescobar11z@gmail.com>
2019-03-15 15:58:35 -0500

    add corrections

--- solution.rb
+++ solution.rb
@@ -3,7 +3,7 @@ get '/' do
   if params[:nombre].nil? || params[:nombre].empty?
   "<h1>Hola desconocido!</h1>"
   else
-  "<h1>Hola #{params[:nombre]}</h1>"
+  "<h1>Hola #{params[:nombre]}!</h1>"
   end
 end
 
